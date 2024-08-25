0d15e4c4b4ed8c9775c913a887c45d5d2ae96d8d
NakshatraAich <mounteverest247@gmail.com>
2024-08-25 12:57:10 +0530

    Added Render Notif

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <h3 className='bg-green-100 text-center font-semibold text-sm'>The backend is hosted on Render Free Tier. It must have gone to sleep, so might take some time to respond.</h3>
     <App />
   </StrictMode>,
 )
